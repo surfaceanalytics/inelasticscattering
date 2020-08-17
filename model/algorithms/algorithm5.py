Fixed some scaling issues in the deconvolution algorithm

model/algorithms/algorithm5.py
@@ -6,8 +6,6 @@ Created on Thu Jul 30 08:55:44 2020
 """
 import numpy as np
 
-
-
 class Algorithm5:
     """
     This algorithm uses: 
@@ -24,18 +22,14 @@ class Algorithm5:
     I: an array to hold the intermediate inelastically scattered spectra
         It has the shape (s, p), where s is the number of spectra and p is the number
         of data points per spectrum.
-    elastic_xsect: a float to represent the elastic scattering cross section of 
-        the scatterer
     inelastic_xsect: a float to represent the inelastic cross section
         density: a float to represent the density of the scatterer
     distance: a float to represent the distance through the scattering medium the 
         electrons must travel
-    norm_factor: a float used in the anglular spread algorithm
-    acceptance_angle: a float used in the angular spread algorithm
+    norm_factor: a float used to rescale the convolved spectra.
     
     Returns
     -------
-    
     inel: an n-d array, representing one lineshape per n events. The data in 
     the arrays can be interpreted as the spectra of the inelastically 
     scattered spectra, where n is the number of inelastic scattering events
@@ -71,11 +65,28 @@ class Algorithm5:
                              'variable':self.norm_factor},]
         
     def _convertDist(self):
-        '''Distance is received in mm but is needed in nm for calculations'''
+        """
+        Converts the distance (received in mm) to nm, which is needed for the
+        calculations.
+
+        Returns
+        -------
+        None.
+
+        """
         self.distance_nm = self.distance * 1000000
 
     def run(self):
+        """
+        This function runs the simulation. It first prepares the needed 
+        factors, then calls a simulation method, depending on the kind of 
+        simulation that should be run.    
 
+        Returns
+        -------
+        Returns a numpy array, representing the simulated spectrum.
+
+        """
         if (self.option == 'film') | (len(self.option) == 0):
             return self._simulateFilm()
         elif self.option == 'bulk':
@@ -108,7 +119,37 @@ class Algorithm5:
 
                 
     def Poisson(self, n, distance, sigma, density):
-        """ This function generates a point in a Poisson distribution"""
+        """
+        This function generates a point in a Poisson distribution.
+
+        Parameters
+        ----------
+        n : INT
+            The Poisson random variable for a Poisson point proccess.
+            In the case of electron scattering, it represents the number of
+            times the electron was scattered.
+        distance : FLOAT or INT
+            Used to calculate lambda in the Poisson point process.
+            In the context of electron scattering, it represents the distance
+            through the scattering medium that the electron travels.
+        sigma : FLOAT
+            Used to calculate lambda in the Poisson point process.
+            In the context of electron scattering, it represents the 
+            scattering cross section of the scatterer.
+        density : FLOAT
+            Used to calculate lambda in the Poisson point process.
+            It represents the density of the scatterer.
+
+        Returns
+        -------
+        p : FLOAT
+            The Poisson probability.
+            In the context of electron scattering, it represents the 
+            probability that an electron is scattered n times when travelling
+            a distance d through the scattering medium, having a scatterer
+            with cross section and density.
+
+        """
         p = ((1/np.math.factorial(n)) 
             * ((distance * density * sigma)**n) 
             * np.exp(-1 * distance * density * sigma))
@@ -122,13 +163,18 @@ class Algorithm5:
         factors. Then the elastically scattered and non-scattered spectra
         are scaled by their respective factors.
         """
-        self._removeMin() 
+        self._removeMin()
         loss = np.flip(self.L)
-
+        ''' Pad the imput spectrum with zeros so that it has the same
+        dimensions as the loss function.
+        '''
         input_spec_padded = np.pad(self.P, (len(loss)-len(self.P),0), 
                                 'constant', constant_values = 0)
-
+        ''' Take Fourier transform of the input spectrum.
+        '''
         fft_input_spec = np.fft.fft(input_spec_padded)
+        ''' Take the Fourier transform of the loss function.
+        '''
         fft_loss = np.fft.fft(loss)
         poisson_factor = self.distance_nm * self.inelastic_xsect * self.density
         norm_factor = self.norm_factor
@@ -136,7 +182,11 @@ class Algorithm5:
         
         exp_factor = np.exp(-1 * poisson_factor)
         
-        fft_total = exp_factor * np.multiply(fft_input_spec, np.exp(total_factor*fft_loss))
+        fft_total = exp_factor * np.multiply(fft_input_spec, 
+                                             np.exp(total_factor*fft_loss))
+        
+        ''' Take the inverse Fourier transform of the convolved spectrum.
+        '''
         total = np.real(np.fft.ifft(fft_total)[-len(self.P):])
         
         self.inel = total - self.P

model/algorithms/algorithm6.py
@@ -6,8 +6,6 @@ Created on Thu Jul 30 08:55:44 2020
 """
 import numpy as np
 
-
-
 class Algorithm6:
     """
     This algorithm deconvolves spectra. It takes a lineshape that represents
@@ -224,23 +222,31 @@ class Algorithm6:
         self._removeMin()
         '''Calculate the Fourier transform of the loss function.'''
         fft_loss = np.fft.fft(np.flip(self.L))
-        for i in range(0,self.n+1):
+        
+        '''for i in range(0,self.n+1):
             new = np.power(fft_loss,i) * self.scale_factors[i]
             new = np.array([new])
             self.I = np.concatenate((self.I,new),axis=0)
-        self.I = np.sum(self.I, axis=0)
+        self.I = np.sum(self.I, axis=0)'''
+        
+        
+        poisson_factor = self.distance_nm * self.inelastic_xsect * self.density
+        norm_factor = self.norm_factor
+        total_factor = poisson_factor * norm_factor
+        fft_multi_loss = np.exp(total_factor*fft_loss)
         
         ''' Pad the input spectrum with zeros so that it has the same 
         dimensions as the loss function.'''
         input_spec_padded = np.pad(self.P, (len(self.L)-len(self.P),0), 
                                 'constant', constant_values = 0)
         fft_input_spec = np.fft.fft(input_spec_padded)
+        exp_factor = np.exp(-1 * poisson_factor)
         
-        fft_deconv = np.divide(fft_input_spec, self.I)
+        fft_deconv = np.divide(fft_input_spec, fft_multi_loss) / exp_factor
         deconv = np.real(np.fft.ifft(fft_deconv))
         deconv = deconv[-len(self.P):]
         
-        self.I = np.real(np.fft.ifft(self.I))
+        self.I = deconv
         
         self._addMin()
      