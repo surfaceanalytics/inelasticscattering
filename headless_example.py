378bb20224bd3495e36c423a510dbab296aef5ac
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2021-09-10 14:37:45 +0200

    Data and headless example for Shirley paper

--- headless_example.py
+++ headless_example.py
@@ -8,49 +8,198 @@ from converters.data_converter import DataConverter
 from model.base_model import MeasuredSpectrum, ScatteringMedium
 from model.algorithms import algorithm5
 from model.model import Model
+from model.base_model import VacuumExcitation
 import matplotlib.pyplot as plt
 import os
 import sys
+import json
 
+#%%
+class SpectrumFigure():
+    def __init__(
+            self,
+            model,
+            params):
+        self.unscattered_spectrum = model.unscattered_spectrum
+        self.scattered_spectrum = model.scattered_spectrum
+        self.simulated_spectrum = model.simulated_spectrum
+        self.loss_function = model.scattering_medium.scatterer.loss_function
+        self.params = params
+        self.shirley_params = params["shirley_params"]
+
+        self.fig, self.axs = plt.subplots(1, 2)
+
+        plt.subplots_adjust(
+            left=0.125,
+            bottom=0.5,
+            right=4.8,
+            top=2,
+            wspace=0.2,
+            hspace=0.2,
+        )
+
+    def plot(self):
+        self.axs[0].plot(
+            self.unscattered_spectrum.x,
+            self.unscattered_spectrum.lineshape)
+        self.axs[0].plot(
+            self.scattered_spectrum.x,
+            self.scattered_spectrum.lineshape)
+        self.axs[0].plot(
+            self.simulated_spectrum.x,
+            self.simulated_spectrum.lineshape)
+        ax0_text = (
+            f"No. of Iterations: {self.params['n_iter']} \n"
+            f"Algorithm Option: {self.params['algorithm_option']} \n\n"
+            f"Norm Factor: {self.params['norm_factor']} \n"
+            )
+        self.axs[0].text(0.1, 0.5,
+                         ax0_text,
+                         horizontalalignment="left",
+                         verticalalignment="top",
+                         transform=self.axs[0].transAxes,
+                         fontsize=8)
+        self.axs[0].legend(["unscattered", "simulated"])
+
+        self.axs[1].plot(self.loss_function.x,
+                         self.loss_function.lineshape)
+        self.axs[1].set_xlim(0, 200)
+        ax1_text = (
+            "Loss function params:\n\n")
+        for key, item in self.shirley_params.items():
+            ax1_text += (
+                f"{key} \n"
+                f"   Edge : {item['edge']} \n"
+                f"   Fermi Width : {item['fermi_width']} \n"
+                f"   Intensity : {item['intensity']} \n"
+                f"   Exponent : {item['exponent']} \n\n"
+                )
+        self.axs[1].text(0.3, 0.9,
+                         ax1_text,
+                         horizontalalignment="left",
+                         verticalalignment="top",
+                         transform=self.axs[1].transAxes,
+                         fontsize=7)
+        self.axs[1].legend(["Shirley Loss Function"])
+
+        plt.tight_layout()
+
+        return self.fig, self.axs
+
+
+#%%
 abspath = os.path.abspath(__file__)
 directory = os.path.dirname(abspath)
 
 directory = os.getcwd()
 
-filename = os.path.join(directory, r'data\test_spectrum.vms')
+spectrum_filename = "SiO2 O1s synthetic"
+
+filename = os.path.join(*[directory,
+                          "data",
+                          spectrum_filename + ".vms"])
 
 converter = DataConverter()
 converter.load(filename)
-data = converter.data[0]
-input_spectrum = MeasuredSpectrum(data['data']['x'], data['data']['y0'])
+
+unscattered_index = 0
+scattered_index = 1
 
 model = Model()
-model.loaded_spectra+=[input_spectrum]
 
-plt.plot(model.loaded_spectra[0].x, model.loaded_spectra[0].lineshape)
+for i in [unscattered_index, scattered_index]:
+    data = converter.data[i]
+    model.loaded_spectra += [MeasuredSpectrum(data['data']['x'],
+                                              data['data']['y0'])]
+model.unscattered_spectrum = model.loaded_spectra[0]
+model.scattered_spectrum = model.loaded_spectra[1]
 
+for spectrum in model.loaded_spectra:
+    plt.plot(spectrum.x, spectrum.lineshape)
+plt.legend(["unscattered", "scattered"])
+plt.show()
 
 filename2 = os.path.join(directory, r'data\scatterers2.json')
 model.loadScatterers(filename2)
 
-model.unscattered_spectrum = model.loaded_spectra[-1]
 
 #%%
-model.scattering_medium.d_through_gas = 10000
+model.scattering_medium.d_through_gas = 10
 model.scattering_medium.pressure = 40
-
 model.setCurrentScatterer('Shirley')
 model.changeAlgorithm(1)
-model.calculation.n_iter = 50
-model.algorithm_option = 'bulk'
-model.scattering_medium.scatterer.norm_factor = 0.6
 
-shirley_params = {'edge':0, 'exponent':0.05, 'fermi_width':0.01}
-model.scattering_medium.scatterer.loss_function.editComponent(0, shirley_params)
+test_params = {
+    "norm_factor": 0.88,
+    "n_iter": 50,
+    "algorithm_option": "bulk",
+    "shirley_params": {
+        "loaded": {
+            "edge": 11.5,
+            "exponent": 0.065,
+            "fermi_width": 20,
+            "intensity" : 1,
+            }
+        }
+    }
+
+model.scattering_medium.scatterer.norm_factor = test_params["norm_factor"]
+model.calculation.n_iter = test_params["n_iter"]
+model.algorithm_option = test_params["algorithm_option"]
+model.scattering_medium.scatterer.loss_function.editComponent(
+    0, test_params["shirley_params"]["loaded"])
+
+vac_exc_params = {
+    "Additional Vac. Excitation 1": {
+        "edge": 11.5,
+        "exponent": 0.065,
+        "fermi_width": 20,
+        "intensity" : 25,
+        },
+    "Additional Vac. Excitation 2": {
+        "edge": 11.5,
+        "exponent": 0.065,
+        "fermi_width": 20,
+        "intensity" : 500,
+        }
+    }
+legend = ["Loaded Shirley"]
+for key, d in vac_exc_params.items():
+    vac_exc = VacuumExcitation(
+        edge=d["edge"],
+        fermi_width=d["fermi_width"],
+        intensity=d["intensity"],
+        exponent=d["exponent"]
+        )
+    model.scattering_medium.scatterer.loss_function.addComponent(vac_exc)
+    legend.append(f"{key}")
+
+model.scattering_medium.scatterer.loss_function.buildLine()
+test_params["shirley_params"].update(vac_exc_params)
+
+
+lf = model.scattering_medium.scatterer.loss_function
+for component in lf.components:
+    y = component.function(lf.x)
+    plt.plot(lf.x, y)
+plt.legend(legend)
+plt.show()
+
+
 
 model.scatterSpectrum()
 
-fig, ax = plt.subplots(1,2)
-ax[0].plot(model.simulated_spectrum.x, model.simulated_spectrum.lineshape)
-ax[1].plot(model.scattering_medium.scatterer.loss_function.x,model.scattering_medium.scatterer.loss_function.lineshape)
-ax[1].set_xlim(0,200)
+spectrum_fig = SpectrumFigure(
+    model=model,
+    params=test_params)
+fig, axs = spectrum_fig.plot()
+plt.plot()
+
+
+
+#%%
+output_folder = r"C:\Users\pielsticker\Lukas\MPI-CEC\Publications\Shirley paper\test figures"
+filenumber = len([f for f in os.listdir(output_folder) if f.startswith(spectrum_filename)])
+fig_filepath = os.path.join(output_folder,
+                            f"{spectrum_filename}_{filenumber}.png")
+fig.savefig(fig_filepath)
