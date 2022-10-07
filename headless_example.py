0b52879da8acb1a6ed23e5f56edaff6ac7305058
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2022-10-07 14:57:14 +0200

    Update headless_example
    - Use different file name.
    - Extract gas-phase-induced region (PEIE) from scattered spectrum.

--- headless_example.py
+++ headless_example.py
@@ -8,7 +8,7 @@ from converters.data_converter import DataConverter
 from model.base_model import MeasuredSpectrum, ScatteringMedium
 from model.algorithms import algorithm5
 from model.model import Model
-from model.base_model import VacuumExcitation
+from model.base_model import VacuumExcitation, Gauss
 import matplotlib.pyplot as plt
 import os
 import sys
@@ -59,14 +59,16 @@ class SpectrumFigure:
         self.axs[1].plot(self.loss_function.x, self.loss_function.lineshape)
         self.axs[1].set_xlim(0, 200)
         ax1_text = "Loss function params:\n\n"
-        for key, item in self.shirley_params.items():
-            ax1_text += (
-                f"{key} \n"
-                f"   Edge : {item['edge']} \n"
-                f"   Fermi Width : {item['fermi_width']} \n"
-                f"   Intensity : {item['intensity']} \n"
-                f"   Exponent : {item['exponent']} \n\n"
-            )
+# =============================================================================
+#         for key, item in self.shirley_params.items():
+#             ax1_text += (
+#                 f"{key} \n"
+#                 f"   Edge : {item['edge']} \n"
+#                 f"   Fermi Width : {item['fermi_width']} \n"
+#                 f"   Intensity : {item['intensity']} \n"
+#                 f"   Exponent : {item['exponent']} \n\n"
+#             )
+# =============================================================================
         self.axs[1].text(
             0.3,
             0.9,
@@ -89,7 +91,7 @@ directory = os.path.dirname(abspath)
 
 directory = os.getcwd()
 
-spectrum_filename = "SiO2 O1s"
+spectrum_filename = "test"
 
 filename = os.path.join(
     *[directory, "data", "Shirley", spectrum_filename + ".vms"]
@@ -116,14 +118,14 @@ for spectrum in model.loaded_spectra:
 plt.legend(["unscattered", "scattered"])
 plt.show()
 
-filename2 = os.path.join(directory, r"data\scatterers2.json")
+filename2 = os.path.join(directory, r"data\scatterers_shirley.json")
 model.loadScatterers(filename2)
 
 
 #%%
 model.scattering_medium.d_through_gas = 10
 model.scattering_medium.pressure = 40
-model.setCurrentScatterer("Shirley")
+model.setCurrentScatterer("ShirleyPlus")
 model.changeAlgorithm(1)
 
 test_params = {
@@ -131,49 +133,47 @@ test_params = {
     "n_iter": 50,
     "algorithm_option": "bulk",
     "shirley_params": {
-        "loaded": {
-            "edge": 11.5,
-            "exponent": 0.065,
-            "fermi_width": 20,
-            "intensity": 1,
+        "edge": 20,
+        "exponent": 0.1,
+        "fermi_width": 20,
+        "intensity": 500,
         }
-    },
-}
+    }
 
 model.scattering_medium.scatterer.norm_factor = test_params["norm_factor"]
 model.calculation.n_iter = test_params["n_iter"]
 model.algorithm_option = test_params["algorithm_option"]
 model.scattering_medium.scatterer.loss_function.editComponent(
-    0, test_params["shirley_params"]["loaded"]
+    0, test_params["shirley_params"]
 )
 
 vac_exc_params = {
     "Additional Vac. Excitation 1": {
-        "edge": 11.5,
-        "exponent": 0.065,
-        "fermi_width": 20,
-        "intensity": 25,
+        "peak_type": Gauss,
+        "position": 35,
+        "width": 10,
+        "intensity": 2,
     },
     "Additional Vac. Excitation 2": {
-        "edge": 11.5,
-        "exponent": 0.065,
-        "fermi_width": 20,
-        "intensity": 500,
+        "peak_type": Gauss,
+        "position": 60,
+        "width": 10,
+        "intensity": 2,
     },
 }
+
 legend = ["Loaded Shirley"]
 for key, d in vac_exc_params.items():
-    vac_exc = VacuumExcitation(
-        edge=d["edge"],
-        fermi_width=d["fermi_width"],
+    vac_exc = d["peak_type"](#VacuumExcitation(
+        position=d["position"],
+        width=d["width"],
         intensity=d["intensity"],
-        exponent=d["exponent"],
     )
     model.scattering_medium.scatterer.loss_function.addComponent(vac_exc)
     legend.append(f"{key}")
 
 model.scattering_medium.scatterer.loss_function.buildLine()
-test_params["shirley_params"].update(vac_exc_params)
+#test_params["shirley_params"].update(vac_exc_params)
 
 
 lf = model.scattering_medium.scatterer.loss_function
@@ -190,13 +190,53 @@ spectrum_fig = SpectrumFigure(model=model, params=test_params)
 fig, axs = spectrum_fig.plot()
 plt.plot()
 
+# =============================================================================
+# output_folder = r"C:\Users\pielsticker\Lukas\MPI-CEC\Publications\Shirley paper\test figures"
+# filenumber = len(
+#     [f for f in os.listdir(output_folder) if f.startswith(spectrum_filename)]
+# )
+# fig_filepath = os.path.join(
+#     output_folder, f"{spectrum_filename}_{filenumber}.png"
+# )
+# fig.savefig(fig_filepath)
+# =============================================================================
 
-#%%
-output_folder = r"C:\Users\pielsticker\Lukas\MPI-CEC\Publications\Shirley paper\test figures"
-filenumber = len(
-    [f for f in os.listdir(output_folder) if f.startswith(spectrum_filename)]
-)
-fig_filepath = os.path.join(
-    output_folder, f"{spectrum_filename}_{filenumber}.png"
-)
-fig.savefig(fig_filepath)
+#%% PEIE paper Tao et al.
+import numpy as np
+
+x = model.unscattered_spectrum.x
+scattered = np.array(model.scattered_spectrum.lineshape)
+unscattered =  np.array(model.unscattered_spectrum.lineshape)
+
+f = np.max(unscattered)/np.max(scattered)
+unscattered /= f
+i = np.argmax(unscattered)
+diff = np.subtract(scattered, unscattered)[:i]
+
+for lineshape in (unscattered, scattered):
+    plt.plot(model.unscattered_spectrum.x, lineshape)
+plt.legend(["unscattered", "scattered"])
+plt.show()
+
+plt.plot(
+    x[:i],
+    diff
+    )
+plt.legend(["diff"])
+plt.show()
+
+new_x = np.flip(np.arange(0, x[i]-x[0], model.unscattered_spectrum.step))
+plt.plot(
+    new_x,
+    diff
+    )
+plt.xlim(np.max(new_x), np.min(new_x))
+plt.text(np.mean(new_x) + 5, 0, f"\u0394E: {np.round(new_x[np.argmax(diff)],2)} eV")
+
+plt.legend(["PEIE"])
+plt.show()
+
+
+
+
+
